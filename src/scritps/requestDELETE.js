1cdd54fa7aabb07f03dad927978f9b9d0f4470d9
Matheus Popolin <matheuspopolin@hotmail.com>
2022-11-09 13:58:05 -0300

    feat/delete

--- src/scritps/requestDELETE.js
+++ src/scritps/requestDELETE.js
@@ -1,17 +1,24 @@
+import { toast } from "./toast.js" 
+
 const baseUrl = "https://m2-api-adot-pet.herokuapp.com/"
 
-export async function deleteProfile(token) {
+export async function deleteProfileApi(token) {
     try {
         const response = await fetch(`${baseUrl}users/profile`, {
             method: 'DELETE',
             headers: {
-                "content-type": "application/json",
-                "Authorization": `Bearer ${token.token}`
+                "Authorization": `Bearer ${token}`
             }
         })
-        return response
+        const responseJson = await response.json()
+        if(response.ok){
+            toast('Usúario deletado com sucesso', 'green')
+            localStorage.removeItem("@userToken")
+            return responseJson;
+        }
     }
     catch (err) {
+        toast('Ocorreu um erro interno', 'red')
         console.log(err);
     }
 }
